d15df6e4911df86c456156456eb844083fed8404
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2022-04-03 20:51:22 +0200

    resizable board & css cleanup

--- docs/main.js
+++ docs/main.js
@@ -1,47 +1,42 @@
-
-class Board{
-	constructor(rows, cols, cellWidth, cellHeight, borderWidth){
+class Game{
+	constructor(cols, rows){
 		this.rows = rows
 		this.cols = cols
-		this.cellSize = [cellWidth, cellHeight]
-		this.bordWid = borderWidth
+
+		this.boardSection = document.createElement("div")
+		this.boardSection.setAttribute("class", "boardSection")
+
+		this.playerSection = document.createElement("div")
+		this.playerSection.setAttribute("class", "playerSection")		
+
+		const game = document.getElementById("gameContainer")
+		game.appendChild(this.boardSection)
+		game.appendChild(this.playerSection)
+
+		this.board = document.createElement("div")
+		this.board.setAttribute("class", "board")
+		this.board.style.width
+
+		this.boardSection.appendChild(this.board)
+
 	}
 	build(){
-		const game = document.getElementById("gameContainer")
-		const board = document.createElement("div")
-		board.setAttribute("class", "board")
-		board.style.position = "absolute"
-		board.style.width = this.cellSize[0]*this.rows+"px"
-		board.style.height = this.cellSize[1]*this.cols+"px"
-		board.style.top = "50%"
-		board.style.left = "50%"
-		board.style.transform = "translate(-50%, -50%)"
 		for(let i=0; i<this.rows; i++){
-			const row = document.createElement("div")
+			const row = document.createElement("row")
 			row.setAttribute("class", "row")
-			row.style.clear = "both"
 
-			game.appendChild(board)
-			board.appendChild(row)
+			this.board.appendChild(row)
+			//this.boardSection.appendChild(document.createElement("br"))
 
-			for(let j=0; j<this.cols; j++){
-				const cell = document.createElement("div")
+			for(let i=0; i<this.cols; i++){
+				const cell = document.createElement("cell")
 				cell.setAttribute("class", "cell")
-				cell.style.float = "left"
-				cell.style.backgroundColor = "white"
-				cell.style.width = (this.cellSize[0]-(this.bordWid*2))+"px"
-				cell.style.height = (this.cellSize[1]-(this.bordWid*2))+"px"
-				if((i+j)%2==0){
-					cell.style.border = this.bordWid+"px solid gray"
-				}
-				else{
-					cell.style.border = this.bordWid+"px solid darkgray"
-				}
+				
 				row.appendChild(cell)
 			}
 		}
 	}
 }
 
-const b = new Board(5,5,100,100,3)
+const b = new Game(4, 4)
 b.build()
